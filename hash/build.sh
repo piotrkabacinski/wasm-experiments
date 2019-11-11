b25d1a6131600e6a6c0b45430f511626251296f1
Piotr Kabaciński <piotrkabacinski@users.noreply.github.com>
2019-11-11 12:59:22 +0100

    Build options comments

--- hash/build.sh
+++ hash/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# -03:
+# https://emscripten.org/docs/optimizing/Optimizing-Code.html
+
+# -fno-rtti -fno-exceptions:
+# https://emscripten.org/_sources/docs/optimizing/Optimizing-Code.txt:
+# C++ run-time type info support (dynamic casts, etc.) adds overhead that is sometimes not needed.
+# For example, in Box2D neither rtti nor exceptions are needed, and if you build the source files with `-fno-rtti -fno-exceptions`
+# then it shrinks the output by 15% (!)
+
+# -s DISABLE_EXCEPTION_CATCHING=1:
+# https://emscripten.org/docs/optimizing/Optimizing-Code.html#c-exceptions
+
 emcc ./src/hash.cpp ./src/sha256/sha256.cpp \
 -s WASM=1 \
 -O3 \
@@ -9,4 +21,3 @@ emcc ./src/hash.cpp ./src/sha256/sha256.cpp \
 -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap', 'getValue']" \
 -o hash.out.js \
 -std=c++14 \
--s ASSERTIONS=0
