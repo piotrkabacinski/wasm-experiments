e3aadac9ee897a33f33e8a8d37650be4ed5452eb
Piotr Kabaciński <piotrkabacinski@users.noreply.github.com>
2019-11-15 22:06:46 +0100

    Create multiple files zip

--- zip/src/zip.cpp
+++ zip/src/zip.cpp
@@ -29,50 +29,27 @@ extern "C"
 
     void read_file(int *filesData, int length)
     {
-        // cout << length << endl;
+        int i = 0;
 
-        // struct zip_t *zip = zip_open("file.zip", ZIP_DEFAULT_COMPRESSION_LEVEL, 'w');
+        struct zip_t *zip = zip_open("file.zip", ZIP_DEFAULT_COMPRESSION_LEVEL, 'w');
 
-        for (int i = 0; i < length; i++)
+        while (i < length)
         {
-            string file_name = get_file_name((int *)filesData[i], (int)filesData[i + 1]);
+            string file_name = get_file_name((int *)filesData[i], filesData[i + 1]);
 
-            cout << i << file_name << endl;
+            zip_entry_open(zip, file_name.c_str());
+            {
+                zip_entry_fwrite(zip, file_name.c_str());
+            }
 
-            // char *file_name = get_file_name(&filesData[i], filesData[++i]).c_str();
-            // zip_entry_open(zip, file_name);
-            // {
-            //     zip_entry_fwrite(zip, file_name);
-            // }
+            zip_entry_close(zip);
+
+            i = i + 2;
         }
 
-        // zip_entry_close(zip);
-        // zip_close(zip);
+        zip_close(zip);
 
-        // call_getZip();
-        // file.close();
-
-        // string file_name = get_file_name(buffer, length);
-        // ifstream file("/" + file_name);
-
-        // if (!file.is_open())
-        // {
-        //     cout << "Failed to open file" << endl;
-        // }
-        // else
-        // {
-        //     struct zip_t *zip = zip_open("file.zip", ZIP_DEFAULT_COMPRESSION_LEVEL, 'w');
-
-        //     zip_entry_open(zip, file_name.c_str());
-        //     {
-        //         zip_entry_fwrite(zip, file_name.c_str());
-        //     }
-        //     zip_entry_close(zip);
-        //     zip_close(zip);
-
-        //     call_getZip();
-        // }
-
-        // file.close();
+        call_getZip();
+        file.close();
     }
 }
