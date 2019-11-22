9c21b7dde75f4e3601f98258482e131e576af589
Piotr Kabaciński <piotrkabacinski@users.noreply.github.com>
2019-11-22 11:58:45 +0100

    Iterations

--- hash/node/index.js
+++ hash/node/index.js
@@ -1,63 +1,75 @@
 const express = require("express");
 const sha256 = require("sha256");
 const Module = require("../hash.out");
+const { randomString, getRandomInt } = require("./randomString");
 
 const app = express();
 
 const divide = 1000000;
-const [, , message, iterations] = process.argv;
+const [, , iterations = "1"] = process.argv;
+
+const times = {
+  sha256: [],
+  wasm: []
+};
+
+const calculateAverage = input =>
+  parseFloat(input.reduce((acc, value) => acc + value) / input.length).toFixed(
+    5
+  );
 
 let wasmStart;
 
-(() => {
-  const sha256Start = process.hrtime();
+const sha256test = message => {
+  const start = process.hrtime();
   const hashedValue = sha256(message);
 
-  console.log(
-    "sha256:",
-    hashedValue,
-    `${process.hrtime(sha256Start)[1] / divide}ms`
-  );
-})();
+  times.sha256.push(process.hrtime(start)[1] / divide);
+};
 
 global.hashed = (pointer, length) => {
-  let hashedValue = "";
+  times.wasm.push(process.hrtime(wasmStart)[1] / divide);
+  //   let hashedValue = "";
+  //   for (let i = 0; i < length; i++) {
+  //     const char = Module.getValue(pointer + i, "i8");
+  //     hashedValue += String.fromCharCode(char);
+  //   }
 
-  for (let i = 0; i < length; i++) {
-    const char = Module.getValue(pointer + i, "i8");
-    hashedValue += String.fromCharCode(char);
+  //   console.log(hashed);
+};
+
+const wasmTest = message => {
+  const encoded = [];
+
+  for (let i = 0; i < message.length; i++) {
+    encoded.push(message.charCodeAt(i));
+  }
+
+  const decoded = new Uint32Array(encoded);
+
+  const buffer = Module._malloc(decoded.length * decoded.BYTES_PER_ELEMENT);
+
+  Module.HEAP32.set(decoded, buffer >> 2);
+
+  wasmStart = process.hrtime();
+
+  Module.ccall("hash", ["number", "number"], "void", [buffer, decoded.length]);
+
+  Module._free(buffer);
+};
+
+Module.onRuntimeInitialized = () => {
+  for (let i = 0; i < Number(iterations); i++) {
+    const message = randomString(getRandomInt(5, 20));
+
+    sha256test(message);
+    wasmTest(message);
   }
 
   console.log(
-    "wasm:",
-    hashedValue,
-    `${process.hrtime(wasmStart)[1] / divide}ms`
+    "sha256:",
+    calculateAverage(times.sha256),
+    "WASM:",
+    calculateAverage(times.wasm)
   );
 };
-
-const main = () => {
-  Module.onRuntimeInitialized = () => {
-    const encoded = [];
-
-    for (let i = 0; i < message.length; i++) {
-      encoded.push(message.charCodeAt(i));
-    }
-
-    const decoded = new Uint32Array(encoded);
-
-    const buffer = Module._malloc(decoded.length * decoded.BYTES_PER_ELEMENT);
-
-    Module.HEAP32.set(decoded, buffer >> 2);
-
-    wasmStart = process.hrtime();
-
-    Module.ccall("hash", ["number", "number"], "void", [
-      buffer,
-      decoded.length
-    ]);
-
-    Module._free(buffer);
-  };
-};
-
-app.listen(3000, main);
---
+++ hash/node/randomString.js
@@ -0,0 +1,18 @@
+const getRandomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min)) + min;
+
+function randomString(length) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+module.exports = {
+  randomString,
+  getRandomInt
+};
